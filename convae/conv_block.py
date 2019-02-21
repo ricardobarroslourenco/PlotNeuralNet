f56c209fc5f99b76d74fd00bf1b9bb3817ab325f
ricardobarroslourenco <ricardobarroslourenco@gmail.com>
2019-02-21 15:48:20 -0600

    conv block test changes

--- convae/conv_block.py
+++ convae/conv_block.py
@@ -41,11 +41,12 @@ arch = [
     to_input('./coast_of_chile_high_res.png'),
 
     # Define a convolution block b01
-    to_ConvRelu('bconvrelu', s_filer=conv_b01_l01_side, n_filer=conv_b01_l01_params,
-            width=(conv_b01_l01_params*scaling_factor), height=(conv_b01_l01_side*scaling_factor), depth=(conv_b01_l01_side*scaling_factor), offset="(0,0,0)", to="(0,0,0)"),
     # Conv01
-    to_Conv('b01l01', s_filer=conv_b01_l01_side, n_filer=conv_b01_l01_params,
-            width=(conv_b01_l01_params*scaling_factor), height=(conv_b01_l01_side*scaling_factor), depth=(conv_b01_l01_side*scaling_factor), offset="(0,0,0)", to="(bconvrelu-east)"),
+    to_Conv('conv_b01_l01', s_filer=conv_b01_l01_side, n_filer=conv_b01_l01_params,
+            width=(conv_b01_l01_params*scaling_factor), height=(conv_b01_l01_side*scaling_factor), depth=(conv_b01_l01_side*scaling_factor), offset="(0,0,0)", to="(0,0,0)"),
+    # Relu01
+    to_Conv('relu_b01_l01', s_filer=relu_b01_l01_side, n_filer=relu_b01_l01_params,
+            width=(relu_b01_l01_params*scaling_factor), height=(relu_b01_l01_side*scaling_factor), depth=(relu_b01_l01_side*scaling_factor), offset="(0,0,0)", to="(conv_b01_l01-east)"),
     to_end()
 ]
 
